5c2cfb6808d77a4669cd9e2fe0c283c7998b9bf0
jinping <jinping@gzxx.com>
2022-02-21 09:43:49 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * Description：测试使用类
+ * CreateTime：2022/2/21 9:39
+ *
+ * @author 金萍
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
